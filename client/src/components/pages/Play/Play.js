06177f7df42ae1ac022cc9f8b21a93af346ba1a9
Caleb Kang <bhk811@gmail.com>
2019-02-10 00:26:13 -0800

    editor

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import AceEditor from "react-ace";
+import "brace/mode/javascript";
+import "brace/theme/tomorrow_night";
+
 import "./play.css";
 var str = "Proident esse mollit ex id eiusmod enim. Adipisicing ad eiusmod dolore cupidatat adipisicing officia non. Excepteur proident est consequat pariatur est aute non exercitation consequat esse id eiusmod. Amet commodo culpa commodo elit exercitation. Sunt ipsum dolor ipsum commodo aliquip ad ullamco velit eu nulla enim nulla cillum duis.";
 console.log(str.length);
@@ -24,26 +28,58 @@ class Play extends Component {
     render() {
         console.log(this.state.percentage);
         return(
-            <div className="play mt-5">
+            <div className="play">
             <div className="row text-center">
-                
                 <div className="col-md-9">
-                    <h5>CLICK START TO BEGIN</h5><button className="btn btn-light btn-sm mb-4">Start <i className="far fa-play-circle"></i></button>
-                    <div className="card mb-4 pb-5">
-                        <div className="card-body text-left text-monospace">
-                            Proident esse mollit ex id eiusmod enim. Adipisicing ad eiusmod dolore cupidatat adipisicing officia non. Excepteur proident est consequat pariatur est aute non exercitation consequat esse id eiusmod. Amet commodo culpa commodo elit exercitation. Sunt ipsum dolor ipsum commodo aliquip ad ullamco velit eu nulla enim nulla cillum duis.
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <textarea className="form-control shadow-sm" rows="6" onKeyUp={this.handleProgressBar} ></textarea>
-                    </div>
+                    <h5>CLICK START TO BEGIN</h5><button className="btn btn-light btn-sm mb-3">Start <i className="far fa-play-circle"></i></button>
+                    <AceEditor 
+                        mode="javascript"
+                        theme="tomorrow_night"
+                        onChange={this.onChange}
+                        name="UNIQUE_ID_OF_DIV"
+                        style={{width: "100%"}}
+                        editorProps={{
+                            $blockScrolling: true
+                        }}
+                        setOptions={{
+                            fontSize: '10pt',
+                            minLines: 10,
+                            maxLines: 10,
+                            readOnly: true,
+                            tabSize: 2
+                        }}
+                    />
+                    <hr className="my-3" />
+                    <AceEditor 
+                        mode="javascript"
+                        theme="tomorrow_night"
+                        onChange={this.onChange}
+                        name="UNIQUE_ID_OF_DIV"
+                        style={{width: "100%"}}
+                        editorProps={{
+                            $blockScrolling: true,
+                            enableLiveAutocompletion: true
+                        }}
+                        setOptions={{
+                            fontSize: '10pt',
+                            minLines: 10,
+                            maxLines: 10,
+                            tabSize: 2
+                        }}
+                    />
                     <div className="progress mt-2">
-                        <div className="progress-bar bg-secondary progress-bar-striped progress-bar-animated shadow" aria-valuemin="0" style={{ width: `${this.state.percentage}%` }}>Jonathan<i className="fas fa-running fa-3x"></i></div>
+                        <div className="progress-bar bg-secondary progress-bar-striped progress-bar-animated shadow" aria-valuemin="0" style={{ width: `${this.state.percentage}%` }}>Jonathan<i className="fas fa-running fa-2x"></i></div>
                     </div>
                 </div>
 
                 <div className="col-md-3">
-                    <div className="alert alert-secondary">
+                    <div className="alert alert-light">
+                        LANGUAGE: JAVASCRIPT
+                    </div>
+                    <div className="alert alert-info" id="prompt">
+                        FOR LOOP
+                    </div>
+                    <div className="alert alert-secondary mt-5">
                         LEADERBOARD
                     </div>
                     <ul className="list-group list-group-flush">
@@ -66,18 +102,6 @@ class Play extends Component {
                         <li className="list-group-item d-flex justify-content-between align-items-center">
                             <i>5.</i>Clint
                             <h5><span className="badge badge-secondary">20.4</span></h5>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <i>6.</i>Caleb
-                            <h5><span className="badge badge-secondary">20.7</span></h5>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <i>7.</i>Tyroo
-                            <h5><span className="badge badge-secondary">21.4</span></h5>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <i>8.</i>Peter
-                            <h5><span className="badge badge-secondary">25.7</span></h5>
                         </li>
                         
                     </ul>
