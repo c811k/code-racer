652bd429c909e9f21ce37ba400780da4f0828aae
Caleb Kang <bhk811@gmail.com>
2019-02-19 15:23:12 -0800

    display  best time fix

--- client/src/components/pages/Profile/Profile.js
+++ client/src/components/pages/Profile/Profile.js
@@ -15,9 +15,11 @@ class Profile extends Component {
     componentWillMount() {
         const {username, time} = this.props;
         this.setState({username, time})
+        
     }
 
     handleLogout = () => {
+        console.log(this.state.username);
         axios.get("/logout").then(() => {
             this.props.getLogin();
             this.props.history.push("/login");
@@ -29,7 +31,7 @@ class Profile extends Component {
             <div class="jumbotron text-center">
                 <h3 class="display-4 mb-5">HELLO, {this.state.username}</h3>
                 <h4 class="lead">WELCOME! ARE YOU READY TO TAKE OVER THE LEADERBOARD TODAY?</h4>
-                <h3 class="btn btn-danger btn-lg rounded-0 shadow mt-5 disabled">YOUR BEST TIME: {this.state.time}</h3>
+                <h3 class="btn btn-danger btn-lg rounded-0 shadow mt-5 disabled">YOUR BEST TIME: {this.state.time} seconds</h3>
                 <hr class="my-4" />
                 <Link to="/play" className="btn btn-light btn-lg rounded-0 shadow mr-4"><i class="far fa-thumbs-up"></i> YES, I'M READY
                 </Link>
