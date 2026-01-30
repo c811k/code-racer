cb4890aeafa6335b2960e09ea7f5e143f2f01a41
Caleb Kang <bhk811@gmail.com>
2026-01-30 09:50:18 -0800

    deployment fix

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use( userRoutes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coderacer", { useNewUrlParser: true });
